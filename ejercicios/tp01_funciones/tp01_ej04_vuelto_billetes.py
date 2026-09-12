6902f9c67d425d10fa77fd2522eed2b2042bf74b
But0o <agustinleonardola@gmail.com>
2026-09-11 22:14:59 -0300

    Optimizacion de codigo

--- ejercicios/tp01_funciones/tp01_ej04_vuelto_billetes.py
+++ ejercicios/tp01_funciones/tp01_ej04_vuelto_billetes.py
@@ -3,33 +3,16 @@ def calcular_vuelto(recibido,compra):
         return -2
     else:
         vuelto = recibido - compra
-
-        billetes_5000 = vuelto // 5000
-        vuelto = vuelto % 5000
-
-        billetes_1000 = vuelto // 1000
-        vuelto = vuelto % 1000
-
-        billetes_500 = vuelto // 500
-        vuelto = vuelto % 500
-
-        billetes_200 = vuelto // 200
-        vuelto = vuelto % 200
-
-        billetes_100 = vuelto // 100
-        vuelto = vuelto % 100
-
-        billetes_50 = vuelto // 50
-        vuelto = vuelto % 50
-
-        billetes_10 = vuelto // 10
-        vuelto = vuelto % 10
-
+        billetes = [5000,1000,500,200,100,50,10]
+        cant_billetes = []
+        for denominacion in billetes:
+            resultado_billetes = vuelto // denominacion
+            cant_billetes.append(resultado_billetes)
+            vuelto = vuelto % denominacion
         if vuelto != 0:
             return -1
         else:
-            return billetes_5000, billetes_1000, billetes_500, billetes_200, billetes_100, billetes_50, billetes_10
-
+            return billetes, cant_billetes
 
 def main():
     compra = int(input("Ingrese el total de la compra: "))
@@ -42,9 +25,9 @@ def main():
     elif resultado == -2:
         print("El dinero no es suficiente")
     else:
-        billetes_5000, billetes_1000, billetes_500, billetes_200, billetes_100, billetes_50, billetes_10 = resultado
-
-        print(f"Billetes de $5000: {billetes_5000} \nBilletes de $1000: {billetes_1000} \nBilletes de $500: {billetes_500} \nBilletes de $200: {billetes_200} \nBilletes de $100: {billetes_100} \nBilletes de $50: {billetes_50} \nBilletes de $10: {billetes_10}")
+        billetes, cant_billetes= resultado
+        for bil,cant in zip(billetes, cant_billetes, strict = True):
+            print(f"Billetes de ${bil}: {cant}")
 
     
 if __name__ == "__main__":
